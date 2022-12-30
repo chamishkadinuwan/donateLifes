d1c34362a58124b1cd5933b6bf539d71a5bd8884
ruwanmx-developer <ruwanmx.developer@gmail.com>
2022-12-30 08:15:30 +0530

    bug fixes in insert

--- connection.php
+++ connection.php
@@ -1,16 +1,12 @@
 <?php
 
-$servername="localhost";
-$user="root";
-$password="";
-$db="donatelife";
+$servername = "localhost";
+$user = "root";
+$password = "";
+$db = "donatelife";
 
-$connection=new mysqli($servername,$user,$password,$db);
+$connection = new mysqli($servername, $user, $password, $db);
 
-if($connection -> connect_error){
+if ($connection->connect_error) {
 	die("connection error");
-}else{
-	echo "connection ok";
 }
-
-?>
