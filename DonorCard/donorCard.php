c0707e6684bf7ef3077f5ca39e5755b46f298ee0
Krishan-S <innocentkrrish123@gmail.com>
2023-01-04 00:42:18 +0530

    DonorCard

---
+++ DonorCard/donorCard.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa"
+        crossorigin="anonymous"></script>
+    <link href="donor_cardcss.css" rel="stylesheet">
+</head>
+<body>
+
+
+
+<!-- Opt. 01 - START -->	
+	<div>
+    <p>
+    Name : Subramaniyam Krishan
+    </p>
+    <p>
+    Age : 23
+    </p>
+<p>
+Blood Group	: B+
+</p>
+    <p> <b>
+    AFTER MY DEATH
+    </b></p>
+    <p>
+    <label for="vehicle1"> A) Any part of my Body</label>
+    <input type="checkbox" id="bparts   " name="vehicle1" value="Bike"> or
+    </p>    
+
+    <label for="vehicle1"> B) My Corness</label>
+    <input type="checkbox" id="bparts   " name="vehicle1" value="Bike">
+    <label for="vehicle2"> Kidnys </label>    
+    <input type="checkbox" id="vehicle2" name="vehicle2" value="Car">
+    <label for="vehicle3"> Heart </label>
+    <input type="checkbox" id="vehicle3" name="vehicle3" value="Boat"> <br>
+    <label for="vehicle3"> Lungs </label>
+    <input type="checkbox" id="vehicle3" name="vehicle3" value="Boat">
+    <label for="vehicle3"> Liver </label>
+    <input type="checkbox" id="vehicle3" name="vehicle3" value="Boat">
+    <label for="vehicle3"> Pancreas </label>
+    <input type="checkbox" id="vehicle3" name="vehicle3" value="Boat">
+    
+  
+    
+
+    <p>
+    <b>May be used for the treatment of others !! </b>
+    </p>
+    <p>
+    Signature :	&nbsp; &nbsp; Date : 05/01/2022
+    </p>
+    <p>
+    In Emergency, Contact :	Subramaniyam
+    </p>
+    <p>
+    Telephone : +94 76 8084 334
+    </p>
+    <p class="last">
+    KEEP THIS CARD WITH YOU AT ALL TIME, PLEASE INFORM YOUR RELATIVES/FAMILY ABOUT YOUR WISH
+    </p>
+				
+	</div>
+	<!-- Opt. 01 - END -->
+
+
+
+<!-- <div class="card px-4 py-3 justify-content-top">
+    <p>
+    NAME : .............................................................................................................
+    </p>
+    <p>
+    AGE : ....................................	BLOOD GROUP	: ................................................
+    </p>
+    <p> <b>
+    AFTER MY DEATH
+    </b></p>
+    <p>
+    A) ANY PART OF MY BODY     OR
+    </p>
+    <p>
+    B) MY CORNESS			  KIDNEYS				HEART			 LUNGS				LIVER			 PANCREAS		
+    </p>
+    <p>
+    SIGNATURE :	......................................	DATE : ..........................................
+    </p>
+    <p>
+    IN EMERGENCY, CONTACT :	............................................................................
+    </p>
+    <p>
+    TEL : ...................................................................................................................
+    </p>
+    <p>
+    KEEP THIS CARD WITH YOU AT ALL TIME, PLEASE INFORM YOUR RELATIVES/FAMILY ABOUT YOUR WISH
+    </p>
+</div> -->
+
+</body>
+</html>
